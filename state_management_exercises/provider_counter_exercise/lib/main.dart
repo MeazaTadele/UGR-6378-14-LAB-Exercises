ea8cc6487d396772818c060ebd33a4d307d32979
meaza tadele <meazialemayehu16@gmail.com>
2024-04-23 18:26:12 +0300

    provider counter exercise

---
+++ state_management_exercises/provider_counter_exercise/lib/main.dart
@@ -0,0 +1,72 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Counter with ChangeNotifier {
+  int _count = 0;
+
+  int get count => _count;
+
+  void increment() {
+    _count++;
+    notifyListeners(); // Notify listeners that the data has changed
+  }
+
+  void decrement() {
+    _count--;
+    notifyListeners();
+  }
+}
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => Counter(), // Provide an instance of Counter
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Counter App',
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter App'),
+      ),
+      
+      body: Center(
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [ Consumer<Counter>(
+          builder: (context, counter, _) => Text(
+            'Count: ${counter.count}',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+        
+        ElevatedButton(
+          onPressed: (){
+            Provider.of<Counter>(context,listen:false).increment();}, 
+          child: Icon(Icons.add)),
+        ElevatedButton(
+          onPressed: (){
+            Provider.of<Counter>(context,listen: false).decrement();} , 
+          child: Icon(Icons.remove))],
+        )
+      ),
+
+    );
+  }
+}
